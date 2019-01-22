3108c5a9b229e4742bd5af163d87248761c33e4f
Alex Vilmur <alex@vilmurnet.com>
2019-01-22 16:08:42 -0600

    Add project files.

---
+++ MarcelDigital.Umbraco.Bootstrap4Grid/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarcelDigital.Umbraco.Bootstrap4Grid
+{
+    public class Class1
+    {
+    }
+}
