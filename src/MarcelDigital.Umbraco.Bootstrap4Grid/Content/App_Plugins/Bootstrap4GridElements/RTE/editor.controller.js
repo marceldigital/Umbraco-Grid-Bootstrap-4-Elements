b5d80e60f7b492d5d62a491912c16a805d746579
Alex Vilmur <vipervf1@users.noreply.github.com>
2019-01-25 11:43:24 -0600

    Added bootstrap 4 RTE. (#10)
    Adds the Bootstrap 4 RTE to the available editors. Allows the user to do a subset of the normal RTE which are bootstrap compatible.

---
+++ src/MarcelDigital.Umbraco.Bootstrap4Grid/Content/App_Plugins/Bootstrap4GridElements/RTE/editor.controller.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    function MdBootstrap4GridElementsRte($scope, tinyMceService, editorState) {
+        var vm = this;
+
+        vm.config = {
+            selector: "textarea",
+            toolbar: ['code', 'styleselect', 'bold', 'italic', 'strikethrough', 'underline', 'alignleft', 'aligncenter', 'alignright', 'bullist', 'numlist', 'link'],
+            stylesheets: ['bootstrap4-elements-formats']
+        };
+        vm.openLinkPicker = openLinkPicker;
+
+        function openLinkPicker(editor, currentTarget, anchorElement) {
+            vm.linkPickerOverlay = {
+                view: 'linkpicker',
+                currentTarget: currentTarget,
+                anchors: tinyMceService.getAnchorNames(JSON.stringify(editorState.current.properties)),
+                show: true,
+                submit: function (model) {
+                    tinyMceService.insertLinkInEditor(editor, model.target, anchorElement);
+                    vm.linkPickerOverlay.show = false;
+                    vm.linkPickerOverlay = null;
+                }
+            };
+        }
+    }
+    angular.module("umbraco").controller("md.bf4gridelements.rte", MdBootstrap4GridElementsRte);
+}());
